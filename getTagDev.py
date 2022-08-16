824050cd32eab08d1dfbf97b9a6b3029d304e064
Quân. Nguyễn Lê Minh <quannlm@vng.com.vn>
2022-08-16 21:40:12 +0700

    chore: Fix file name error in getTagDev.py

--- getTagDev.py
+++ getTagDev.py
@@ -4,9 +4,9 @@ import os
 
 # Open the file and load the file
 def getTag():
-    with open('deployment.yaml') as f:
+    with open('docker-compose.yaml') as f:
         data = yaml.load(f, Loader=SafeLoader)
-        tag = data['spec']['template']['spec']['containers'][0]['image']
+        tag = data['services']['java-assignment']['image']
         tag = tag.split(':')
         return tag[1]
 print(getTag())
