325d609b2b3931574129b8d867f4f2708052f238
Quân. Nguyễn Lê Minh <quannlm@vng.com.vn>
2022-08-16 17:11:48 +0700

    chore: Set up CI for Stage branch & update content at home page

---
+++ exportTagStage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export TAG=$(python3 getTagStage.py)
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	String home() {
-		return "Spring is here!";
+		return "Another content!";
 	}
 
 	public static void main(String[] args) {
