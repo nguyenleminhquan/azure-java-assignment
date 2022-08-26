6b092ac62f735c79c2ce86248af035186409befb
Quân. Nguyễn Lê Minh <quannlm@vng.com.vn>
2022-08-26 07:06:30 +0700

    chore: Update dev pipelines

--- stageQA.js
+++ stageQA.js
@@ -3,8 +3,8 @@ const { exit } = require('process')
 
 const config = {
   headers: {
-    'Authorization': 'Basic aGF1dHAyQHZuZy5jb20udm46Z2Fhbmg1dWNqbjdsbnVzdDNwNTVqZzNkaXd5MzR6eWZ3dGFvajR5cHB5a2JzNjZvYjdqYQ==',
-    'Cookie': 'VstsSession=%7B%22PersistentSessionId%22%3A%22df5931cb-d189-468b-bc13-90c7bd116287%22%2C%22PendingAuthenticationSessionId%22%3A%2200000000-0000-0000-0000-000000000000%22%2C%22CurrentAuthenticationSessionId%22%3A%2200000000-0000-0000-0000-000000000000%22%2C%22SignInState%22%3A%7B%7D%7D',
+    'Authorization': `Basic ${process.argv[2]}`,
+    'Cookie': `${process.argv[3]}`,
     'Content-Type': 'application/json'
   }
 }
@@ -12,7 +12,7 @@ const config = {
 const APIGetCount = 'https://dev.azure.com/mps-hautp2/demo-azure/_apis/build/builds?api-version=6.0&statusFilter=inProgress'
 const APIGetStatus = 'https://dev.azure.com/mps-hautp2/demo-azure/_apis/build/builds?api-version=6.0&$top=1'
 
-var intervel = setInterval(checkCount, 1000)
+var intervel = setInterval(checkCount, 2000)
 
 function checkStatus() {
   axios.get(APIGetStatus, config)
@@ -40,6 +40,3 @@ function checkCount() {
   })
   .catch(err => console.log(err))
 }
-
-
-
